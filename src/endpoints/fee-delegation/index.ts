a23d42ca329f556466aebfc1716f6706633ad8fd
Hakko <47938592+dividenconquer@users.noreply.github.com>
2025-08-25 04:42:19 +0900

    feat: 🎸 balance

--- src/endpoints/fee-delegation/index.ts
+++ src/endpoints/fee-delegation/index.ts
@@ -3,9 +3,11 @@ import { fromHono } from "chanfana";
 import { ContextType } from "@/types";
 import { Hono } from "hono";
 import { RelayFeePay } from "./relay";
+import { FeePayerBalance } from "./balance";
 
 const feeDelegationRoutes = fromHono(new Hono<ContextType>());
 
 feeDelegationRoutes.post("/relay", RelayFeePay);
+feeDelegationRoutes.get("/balance", FeePayerBalance);
 
 export default feeDelegationRoutes;
