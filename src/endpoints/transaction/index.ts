23c2092efb763297d7b30639ad28cf7f0cf7523c
Hakko <47938592+dividenconquer@users.noreply.github.com>
2025-08-24 20:16:04 +0900

    feat: 🎸 update

--- src/endpoints/transaction/index.ts
+++ src/endpoints/transaction/index.ts
@@ -2,9 +2,10 @@ import { fromHono } from "chanfana";
 
 import { ContextType } from "@/types";
 import { Hono } from "hono";
+import { Deposit } from "./deposit";
 
 const transactionRoutes = fromHono(new Hono<ContextType>());
 
-transactionRoutes.post("/deposit", UserMe);
+transactionRoutes.post("/deposit", Deposit);
 
 export default transactionRoutes;
