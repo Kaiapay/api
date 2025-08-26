90296eb71cc403cbf1ff82abd2a51f07ed6cf236
Hakko <47938592+dividenconquer@users.noreply.github.com>
2025-08-26 13:11:37 +0900

    feat: 🎸 CHECKED

--- scripts/local_secret_store_setting.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-npx wrangler secrets-store secret create local --name PRIVY_APP_ID --scopes workers --value "cmel98g0x02u6l40btr617b87"
-npx wrangler secrets-store secret create local --name PRIVY_APP_SECRET --scopes workers --value "67R5LAGSUQ5FieLiidhHFS2hDVSseMNH1bkzZRJHUU3TkYTNt44gKFr9oBoGTVGjzPX3z35fY3AyczBmTbZGeAge"
-npx wrangler secrets-store secret create local --name FEEPAYER_PK --scopes workers --value "0x9c984711c9f01801a609ba861c4702ffc940655f576bf1dfc06597ebd2aa7212"
-
